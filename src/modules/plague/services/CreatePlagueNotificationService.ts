926a7a128673163eccc3cf9f00e91fb392fad98d
LuanLucasGon <lucasluangon@gmail.com>
2021-09-25 20:43:25 -0300

    Fix: Mudando nome de arquivos errados

--- src/modules/plague/services/CreatePlagueNotificationService.ts
+++ src/modules/plague/services/CreatePlagueNotificationService.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import Farm from '@modules/farm/schemas/Farm';
-import PlagueNotification from '../schemas/plagueNotification';
+import PlagueNotification from '../schemas/PlagueNotification';
 
 interface IRequestDTO {
     message: string;
