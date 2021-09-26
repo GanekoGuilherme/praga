f24bdb3bd23f8c1811c3c55afc389271439e6a30
LuanLucasGon <lucasluangon@gmail.com>
2021-09-26 19:59:01 -0300

    Feat: Criando os dados do front

--- src/modules/plague/services/FronListService.ts
+++ src/modules/plague/services/FronListService.ts
@@ -11,7 +11,6 @@ class FrontListService {
             return listWithOlnyName.indexOf(item) == pos;
         });
         let type = listWithoutRepeatName.length
-        const plagueNameList2 = await Plague.find().select('name -_id');
         var counts = {};
         for (var i = 0; i < listWithOlnyName.length; i++){
           counts[listWithOlnyName[i]] = (counts[listWithOlnyName[i]] + 1) || 1;
@@ -22,14 +21,20 @@ class FrontListService {
           const percent = (result[i][1])/(plagueList.length)
           a.push({type: result[i][0], percent: percent})
         }
-        const dateEnd = Date.now();
-        const dateBegin = new Date();
-        dateBegin.setHours(dateBegin.getHours() - 24*7);
-        const plagueListDate = await Plague.find({  createdAt: { $gte: (dateBegin ? dateBegin : '0001-01-01'), $lt: (dateEnd ? dateEnd : '2100-01-01') } }).select('-photo -createdAt -updatedAt')
-        console.log(plagueListDate)
+        const b = []
+        for (var i = 1; i < 8; i++){
+          const dateEnd = new Date();
+          dateEnd.setHours(dateEnd.getHours() - 24*(i-1))
+          const dateBegin = new Date();
+          dateBegin.setHours(dateBegin.getHours() - 24*i);
+          const plagueListDate = await Plague.find({  createdAt: { $gte: (dateBegin ? dateBegin : '0001-01-01'), $lt: (dateEnd ? dateEnd : '2100-01-01') } }).select('-photo -updatedAt')
+          b.push({date: dateEnd, qtd: plagueListDate.length})
+        }
+        console.log(b)
         const item = {
           totalPlague: plagueList.length,
           percentList: a,
+          caseDays: b,
           plagueType: type
         }
         return item;
