ca65b3c50b46689c0c492c7b3d09d1073c82e03e
LuanLucasGon <lucasluangon@gmail.com>
2021-09-26 19:38:53 -0300

    Feat: Criando os dados pro front end, pt 3

--- src/modules/plague/services/FronListService.ts
+++ src/modules/plague/services/FronListService.ts
@@ -11,25 +11,25 @@ class FrontListService {
             return listWithOlnyName.indexOf(item) == pos;
         });
         let type = listWithoutRepeatName.length
-        //criar uma forma de retornar a porcentagem
-        console.log(listWithOlnyName)
-        const arroz = [];
-        listWithOlnyName.map(
-          (
-            item
-          ) => {
-            arroz.find((a) => a.name === item);
-            arroz.push();
-          }
-        )
-
+        const plagueNameList2 = await Plague.find().select('name -_id');
         var counts = {};
-        for (var i = 0; i < listWithOlnyName.length; i++)
+        for (var i = 0; i < listWithOlnyName.length; i++){
           counts[listWithOlnyName[i]] = (counts[listWithOlnyName[i]] + 1) || 1;
-        console.log(counts)
-        //agora criar um obejto com o nome da chave e a porcentagem
+        }
+        var result = Object.keys(counts).map((key) => [String(key), counts[key]]);
+        const a = [];
+        for (var i = 0; i < result.length; i++){
+          const percent = (result[i][1])/(plagueList.length)
+          a.push({type: result[i][0], percent: percent})
+        }
+        const dateEnd = Date.now();
+        const dateBegin = new Date();
+        dateBegin.setHours(dateBegin.getHours() - 24*7);
+        const plagueListDate = await Plague.find({  createdAt: { $gte: (dateBegin ? dateBegin : '0001-01-01'), $lt: (dateEnd ? dateEnd : '2100-01-01') } }).select('-photo -createdAt -updatedAt')
+        console.log(plagueListDate)
         const item = {
           totalPlague: plagueList.length,
+          percentList: a,
           plagueType: type
         }
         return item;
