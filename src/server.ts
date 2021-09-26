68486ee958cc793ebfc6dac930a1d8b878613a07
GanekoGuilherme <guilhermeganeko@hotmail.com>
2021-09-26 12:43:30 -0300

    Fix: Removendo imagens para limpar o banco.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import 'express-async-errors';
 
@@ -11,7 +12,7 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-
+app.use('/uploads',express.static(path.join(__dirname, '..', 'uploads')));
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {   
     console.log(error);
     HandleError.handleError({ error, request, response });
