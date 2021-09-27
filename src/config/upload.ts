8a29c8d6800d780a89c3f7b35002209cf41faf8c
GanekoGuilherme <guilhermeganeko@hotmail.com>
2021-09-26 21:36:17 -0300

    Fix: images urls.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,12 +1,14 @@
 import { request } from 'express';
 import multer from 'multer';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default{
     storage: multer.diskStorage({
         destination: path.join(__dirname, '..','..','uploads'),
         filename: (request,file,cb) =>{
-            const fileName = `${Date.now()}-${file.originalname}`;
+            const fileName = `${Date.now()}-${uuidv4()}`;
             cb(null,fileName);
         },
     })
